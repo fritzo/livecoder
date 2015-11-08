0435511c06ddd5faa3709e6f279ea18da24ba181
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2015-11-07 19:42:27 -0800

    Initial pypi setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# livecoder
-A simple python library to support live coding
+# Livecoder
+
+A simple library to support live coding in python.
+Modeled after the idioms in [livecoder.net](http://livecoder.net).
+
+## License
+
+Apache 2.0.
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+__version__ = '0.0.0'
+
+try:
+    with open('README.rst') as f:
+        long_description = f.read()
+except IOError:
+    long_description = 'Simple library to support live coding'
+
+config = {
+    'name': 'livecoder',
+    'version': __version__,
+    'description': 'Simple library to support live coding',
+    'long_description': long_description,
+    'author': 'Fritz Obermeyer',
+    'author_email': 'fritz.obermeyer@gmail.com',
+    'url': 'https://github.com/fritzo/livecoder',
+    'py_modules': ['livecoder'],
+}
+
+setup(**config)
